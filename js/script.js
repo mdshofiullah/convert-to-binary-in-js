ebdbd68dc5c0d47996b6ecbcc5b0b1c8587f7634
mdshofiullah <mdshofiullah65@gmail.com>
2026-05-24 03:00:04 +0600

    Decimal number to Binary Number completed code

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+const decimalToBinary = (input) => {
+  if (input === 0) {
+    return "0";
+  } else if (input === 1) {
+    return "1";
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+  }
+};
+
+const checkUserInput = () => {
+  if (
+    !numberInput.value ||
+    isNaN(parseInt(numberInput.value)) ||
+    parseInt(numberInput.value) < 0
+  ) {
+    alert("Please provide a decimal number greater than or equal to 0");
+    return;
+  }
+
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
